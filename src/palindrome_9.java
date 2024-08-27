8f896dc5f5f83c2648fb5d5fba38d47b7e4b31bc
Tushar <upadhyaytushar04@gmail.com>
2024-08-27 11:09:30 +0530

    palindrome modified

--- src/palindrome_9.java
+++ src/palindrome_9.java
@@ -1,28 +1,25 @@
 public class palindrome_9 {
     public boolean isPalindrome(int x) {
-        if (x == 0) return true;
-
-        if (x < 0 || x % 10 == 0) return false;
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
 
         int reverse = 0;
         while (x > reverse) {
-            int pop = x % 10;
+            int og = x % 10;
             x /= 10;
 
-            reverse = (reverse * 10) + pop;
+            reverse = (reverse * 10) + og;
         }
         return x == reverse || x == reverse / 10;
     }
 
     public static void main(String[] args) {
-        palindrome_9 solution = new palindrome_9();
+        palindrome_9 obj = new palindrome_9();
 
-        int num1 = 121;
-        int num2 = -121;
-        int num3 = 10;
+        // Test cases
+        int[] testCases = {121, -121, 10, 12321, 0};
 
-        System.out.println(num1 + " is palindrome: " + solution.isPalindrome(num1));  // true
-        System.out.println(num2 + " is palindrome: " + solution.isPalindrome(num2));  // false
-        System.out.println(num3 + " is palindrome: " + solution.isPalindrome(num3));  // false
+        for (int num : testCases) {
+            System.out.println(num + " is palindrome: " + obj.isPalindrome(num));
+        }
     }
 }
