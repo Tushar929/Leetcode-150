1702d56c976d848cc13121976a0fec4e2bc0802c
Tushar <upadhyaytushar04@gmail.com>
2024-09-07 18:16:39 +0530

    longestCommonPrefix_14 added

---
+++ src/longestCommonPrefix_14.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class longestCommonPrefix_14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int i = 0;
+        while (i < first.length() && i < last.length() && first.charAt(i) == last.charAt(i)) {
+            i++;
+        }
+        return first.substring(0, i);
+    }
+
+    public static void main(String[] args) {
+        longestCommonPrefix_14 solution = new longestCommonPrefix_14();
+        String[] strs = {"flower", "flow", "flight"};
+        String result = solution.longestCommonPrefix(strs);
+        System.out.println("Longest Common Prefix: " + result);
+    }
+}
